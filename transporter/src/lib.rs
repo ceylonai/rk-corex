93043949fbf36671c50a13856fc0511d4a56b7ef
dewmal <dewmal@egreen.io>
2023-03-04 23:16:32 +0530

    Create ping server

---
+++ transporter/src/lib.rs
@@ -0,0 +1,36 @@
+use futures::StreamExt;
+use libp2p::{identity, Multiaddr, PeerId, Swarm};
+use libp2p::ping::Behaviour;
+use libp2p::swarm::SwarmEvent;
+
+pub async fn start_transporter(tx: tokio::sync::mpsc::Sender<String>) {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {local_peer_id:?}");
+
+    let transport = libp2p::development_transport(local_key).await.unwrap();
+
+    let mut swarm = Swarm::with_async_std_executor(transport, Behaviour::default(), local_peer_id);
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse().unwrap();
+        swarm.dial(remote).unwrap();
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(event) => {
+                tx.send(format!("{:?}", event)).await.unwrap();
+            }
+            _ => {}
+        }
+    }
+}
